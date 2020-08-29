c0a0f366816a4c6a07413d153071ac8df9130464
LarryPenn <49259555+LarryPenn@users.noreply.github.com>
2020-08-29 13:55:45 -0700

    added heapSort algorithm and visualizer

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -20,11 +20,12 @@ import React from 'react';
 import { getMergeSortAnimations } from '../sortingAlgorithms/sortingAlgorithms.js';
 import { getQuickSortAnimations } from '../sortingAlgorithms/sortingAlgorithms.js';
 import { getBubbleSortAnimations } from '../sortingAlgorithms/sortingAlgorithms.js';
+import { getHeapSortAnimations } from '../sortingAlgorithms/sortingAlgorithms.js';
 import './SortingVisualizer.css';
 
 
 // Change this value for the speed of the animations.
-const ANIMATION_SPEED_MS = 200;
+const ANIMATION_SPEED_MS = 20;
 
 // Change this value for the number of bars (value) in the array.
 const NUMBER_OF_ARRAY_BARS = 20;
@@ -35,156 +36,183 @@ const PRIMARY_COLOR = 'turquoise';
 // This is the color of array bars that are being compared throughout the animations.
 const SECONDARY_COLOR = 'red';
 
-export default class SortingVisualizer extends React.Component{
-    constructor(props){
-        super(props);
+export default class SortingVisualizer extends React.Component {
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            array: [],
-            array2: [],
-            array3: [],
-            array4: [],
-        };
+    this.state = {
+      array: [],
+      array2: [],
+      array3: [],
+      array4: [],
+    };
+  }
+
+
+  componentDidMount() {
+    this.resetArray();
+  }
+
+  resetArray() {
+    const array = [];
+    for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
+      array.push(randomIntFromInterval(5, 350));
     }
+    const array2 = array.slice();
+    const array3 = array.slice();
+    const array4 = array.slice();
+    this.setState({ array });
+    this.setState({ array2 });
+    this.setState({ array3 });
+    this.setState({ array4 });
+    //this.setState({array});
+  }
 
-
-    componentDidMount() {
-        this.resetArray();
-    }
-
-    resetArray() {
-        const array = [];
-        for(let i=0;i<20;i++){
-            array.push(randomIntFromInterval(5,350));
-        }
-        const array2 = array;
-        const array3 = array;
-        const array4 = array;
-        this.setState({array});
-        this.setState({array2});
-        this.setState({array3});
-        this.setState({array4});                
-        //this.setState({array});
-    }
-
-    mergeSort() {
-        const animations = getMergeSortAnimations(this.state.array2);
-        for (let i = 0; i < animations.length; i++) {
-          console.log(animations[i]);  
-          const arrayBars = document.getElementsByClassName('array-bar');
-          const isColorChange = i % 3 !== 2;
-          if (isColorChange) {
-            const [barOneIdx, barTwoIdx] = animations[i];
-            const barOneStyle = arrayBars[barOneIdx].style;
-            const barTwoStyle = arrayBars[barTwoIdx].style;
-            const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
-            setTimeout(() => {
-              barOneStyle.backgroundColor = color;
-              barTwoStyle.backgroundColor = color;
-            }, i * ANIMATION_SPEED_MS);
-          } else {
-            setTimeout(() => {
-              const [barOneIdx, newHeight] = animations[i];
-              const barOneStyle = arrayBars[barOneIdx].style;
-              barOneStyle.height = `${newHeight}px`;
-            }, i * ANIMATION_SPEED_MS);
-          }
-        }
+  mergeSort() {
+    const animations = getMergeSortAnimations(this.state.array);
+    for (let i = 0; i < animations.length; i++) {
+      console.log(animations[i]);
+      const arrayBars = document.getElementsByClassName('array-bar');
+      const isColorChange = i % 3 !== 2;
+      if (isColorChange) {
+        const [barOneIdx, barTwoIdx] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+        const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i * ANIMATION_SPEED_MS);
+      } else {
+        setTimeout(() => {
+          const [barOneIdx, newHeight] = animations[i];
+          const barOneStyle = arrayBars[barOneIdx].style;
+          barOneStyle.height = `${newHeight}px`;
+        }, i * ANIMATION_SPEED_MS);
       }
-
-      quickSort() {
-        // We leave it as an exercise to the viewer of this code to implement this method.
-    
-        const animations = getQuickSortAnimations(this.state.array);
-        for (let i = 0; i < animations.length; i++) {
-        //   console.log(animations[i]);
-          const arrayBars = document.getElementsByClassName('array-bar2');
-          setTimeout(() => {
-            const [barOneIdx, barTwoIdx, barOneHeight, barTwoHeight] = animations[i];
-            console.log("Hello");
-            console.log(barOneIdx);
-            console.log(barTwoIdx);            
-            const barOneStyle = arrayBars[barOneIdx].style;
-            const barTwoStyle = arrayBars[barTwoIdx].style;
-            barOneStyle.height = `${barOneHeight}px`;
-            barTwoStyle.height = `${barTwoHeight}px`;
-          }, i * ANIMATION_SPEED_MS);
-        }
     }
+  }
 
+  quickSort() {
+    // We leave it as an exercise to the viewer of this code to implement this method.
 
-    bubbleSort() {
-        // We leave it as an exercise to the viewer of this code to implement this method.
-    
-        const animations = getBubbleSortAnimations(this.state.array);
-        for (let i = 0; i < animations.length; i++) {
-          console.log(animations[i]);
-          const arrayBars = document.getElementsByClassName('array-bar3');
-          setTimeout(() => {
-            const [barOneIdx, barTwoIdx, barOneHeight, barTwoHeight] = animations[i];
-            const barOneStyle = arrayBars[barOneIdx].style;
-            const barTwoStyle = arrayBars[barTwoIdx].style;
-            barOneStyle.height = `${barOneHeight}px`;
-            barTwoStyle.height = `${barTwoHeight}px`;
-          }, i * ANIMATION_SPEED_MS);
-        }
+    const animations = getQuickSortAnimations(this.state.array2);
+    for (let i = 0; i < animations.length; i++) {
+      //   console.log(animations[i]);
+      const arrayBars = document.getElementsByClassName('array-bar2');
+      setTimeout(() => {
+        const [barOneIdx, barTwoIdx, barOneHeight, barTwoHeight] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+        barOneStyle.height = `${barOneHeight}px`;
+        barTwoStyle.height = `${barTwoHeight}px`;
+      }, i * ANIMATION_SPEED_MS);
     }
-    
-    allSort() {
+  }
 
-        //this.mergeSort();
-        this.bubbleSort();
-        this.quickSort();        
+  bubbleSort() {
+    // We leave it as an exercise to the viewer of this code to implement this method.
+
+    const animations = getBubbleSortAnimations(this.state.array3);
+    for (let i = 0; i < animations.length; i++) {
+      console.log(animations[i]);
+      const arrayBars = document.getElementsByClassName('array-bar3');
+      setTimeout(() => {
+        const [barOneIdx, barTwoIdx, barOneHeight, barTwoHeight] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+        barOneStyle.height = `${barOneHeight}px`;
+        barTwoStyle.height = `${barTwoHeight}px`;
+      }, i * ANIMATION_SPEED_MS);
     }
+  }
 
 
-        render() {
-            const { array } = this.state;
-            const {array2} = this.state;
-            const { array3 } = this.state;
+  heapSort() {
+    // We leave it as an exercise to the viewer of this code to implement this method.
 
-        
-            return (
-              <div className="array-container">
-                {array.map((value, idx) => (
-                  <div
-                    className="array-bar"
-                    key={idx}
-                    style={{
-                      backgroundColor: PRIMARY_COLOR,
-                      height: `${value}px`,
-                    }}></div>                
-                ))}
-                {array2.map((value, idx) => (
-                  <div
-                    className="array-bar2"
-                    key={idx}
-                    style={{
-                      backgroundColor: PRIMARY_COLOR,
-                      height: `${value}px`,
-                    }}></div>                
-                ))}     
-                {array3.map((value, idx) => (
-                  <div
-                    className="array-bar3"
-                    key={idx}
-                    style={{
-                      backgroundColor: PRIMARY_COLOR,
-                      height: `${value}px`,
-                    }}></div>                
-                ))}                             
-                <button onClick={() => this.resetArray()}>Generate New Array</button>
-                <button onClick={() => this.mergeSort()}>Merge Sort1</button>
-                <button onClick={() => this.quickSort()}>Quick Sort2</button>
-                <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
-                <button onClick={() => this.allSort()}>All Sort</button>                
-              </div>
-            );
-          }
+    const animations = getHeapSortAnimations(this.state.array);
+    for (let i = 0; i < animations.length; i++) {
+      //   console.log(animations[i]);
+      const arrayBars = document.getElementsByClassName('array-bar');
+      setTimeout(() => {
+        const [barOneIdx, barTwoIdx, barOneHeight, barTwoHeight] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+        barOneStyle.height = `${barOneHeight}px`;
+        barTwoStyle.height = `${barTwoHeight}px`;
+      }, i * ANIMATION_SPEED_MS);
+    }
+  }
+
+
+  allSort() {
+
+    this.mergeSort();
+    this.bubbleSort();
+    this.quickSort();
+    this.heapSort();
+  }
+
+
+  render() {
+    const { array } = this.state;
+    const { array2 } = this.state;
+    const { array3 } = this.state;
+    const { array4 } = this.state;
+
+
+    return (
+      <div className="array-container">
+        {array.map((value, idx) => (
+          <div
+            className="array-bar"
+            key={idx}
+            style={{
+              backgroundColor: PRIMARY_COLOR,
+              height: `${value}px`,
+            }}></div>
+        ))}
+        {/* {array2.map((value, idx) => (
+          <div
+            className="array-bar2"
+            key={idx}
+            style={{
+              backgroundColor: PRIMARY_COLOR,
+              height: `${value}px`,
+            }}></div>
+        ))}
+        {array3.map((value, idx) => (
+          <div
+            className="array-bar3"
+            key={idx}
+            style={{
+              backgroundColor: PRIMARY_COLOR,
+              height: `${value}px`,
+            }}></div>
+        ))}
+        {array4.map((value, idx) => (
+          <div
+            className="array-bar4"
+            key={idx}
+            style={{
+              backgroundColor: PRIMARY_COLOR,
+              height: `${value}px`,
+            }}></div>
+        ))} */}
+        <button onClick={() => this.resetArray()}>Generate New Array</button>
+        <button onClick={() => this.mergeSort()}>Merge Sort1</button>
+        <button onClick={() => this.quickSort()}>Quick Sort2</button>
+        <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+        <button onClick={() => this.heapSort()}>Heap Sort</button>
+        <button onClick={() => this.allSort()}>All Sort</button>
+      </div>
+    );
+  }
 }
 
 
-function randomIntFromInterval(min,max){
-    return Math.floor(Math.random()*(max-min+1) + min);
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
--- src/sortingAlgorithms/sortingAlgorithms.js
+++ src/sortingAlgorithms/sortingAlgorithms.js
@@ -4,13 +4,13 @@ export function getBubbleSortAnimations(array) {
   //console.log(array);
 
 
-  for(let i=0;i<array.length-1;i++){
-    for(let j=0;j<array.length-i-1;j++){
-      if(array[j]>array[j+1]){
-        let temp = array[j+1];
-        array[j+1] = array[j];
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j + 1];
+        array[j + 1] = array[j];
         array[j] = temp;
-        animations.push([j,j+1,array[j],array[j+1]])  
+        animations.push([j, j + 1, array[j], array[j + 1]])
       }
     }
   }
@@ -27,127 +27,179 @@ export function getQuickSortAnimations(array) {
   return animations;
 }
 
-export function getMergeSortAnimations(aarray) {
-    const animations = [];
-    if (aarray.length <= 1) return aarray;
-    const auxiliaryArray = aarray.slice();
-    mergeSortHelper(aarray, 0, aarray.length - 1, auxiliaryArray, animations);
-    return animations;
+export function getMergeSortAnimations(array) {
+  const animations = [];
+  if (array.length <= 1) return array;
+  const auxiliaryArray = array.slice();
+  mergeSortHelper(array, 0, array.length - 1, auxiliaryArray, animations);
+  return animations;
+}
+
+export function getHeapSortAnimations(arr) {
+  let n = arr.length;
+  const animations = [];
+  if (arr.length <= 1) return arr;
+
+  // Build heap (rearrange array) 
+  for (let i = n / 2 - 1; i >= 0; i--)
+    heapify(arr, n, i, animations);
+
+  // One by one extract an element from heap 
+  for (let i = n - 1; i > 0; i--) {
+    // Move current root to end 
+    let temp = arr[0];
+    arr[0] = arr[i];
+    arr[i] = temp;
+    animations.push([0, i, arr[0], arr[i]]);
+
+    // call max heapify on the reduced heap 
+    heapify(arr, i, 0, animations);
   }
-  
-  function mergeSortHelper(
-    mainArray,
-    startIdx,
-    endIdx,
-    auxiliaryArray,
-    animations,
-  ) {
-    if (startIdx === endIdx) return;
-    const middleIdx = Math.floor((startIdx + endIdx) / 2);
-    mergeSortHelper(auxiliaryArray, startIdx, middleIdx, mainArray, animations);
-    mergeSortHelper(auxiliaryArray, middleIdx + 1, endIdx, mainArray, animations);
-    doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, animations);
+
+  //console.log(animations);
+  //console.log(arr);
+
+  return animations;
+
+}
+
+function heapify(arr, n, i, animations) {
+  let largest = i; // Initialize largest as root 
+  let l = 2 * i + 1; // left = 2*i + 1 
+  let r = 2 * i + 2; // right = 2*i + 2 
+
+  // If left child is larger than root 
+  if (l < n && arr[l] > arr[largest])
+    largest = l;
+
+  // If right child is larger than the largest so far
+  if (r < n && arr[r] > arr[largest])
+    largest = r;
+
+  // If largest is not root 
+  if (largest !== i) {
+    let swap = arr[i];
+    arr[i] = arr[largest];
+    arr[largest] = swap;
+    animations.push([i, largest, arr[i], arr[largest]]);
+
+    // Recursively heapify the affected sub-tree 
+    heapify(arr, n, largest, animations);
   }
-  
-  function doMerge(
-    mainArray,
-    startIdx,
-    middleIdx,
-    endIdx,
-    auxiliaryArray,
-    animations,
-  ) {
-    let k = startIdx;
-    let i = startIdx;
-    let j = middleIdx + 1;
-    while (i <= middleIdx && j <= endIdx) {
-      // These are the values that we're comparing; we push them once
-      // to change their color.
-      animations.push([i, j]);
-      // These are the values that we're comparing; we push them a second
-      // time to revert their color.
-      animations.push([i, j]);
-      if (auxiliaryArray[i] <= auxiliaryArray[j]) {
-        // We overwrite the value at index k in the original array with the
-        // value at index i in the auxiliary array.
-        animations.push([k, auxiliaryArray[i]]);
-        mainArray[k++] = auxiliaryArray[i++];
-      } else {
-        // We overwrite the value at index k in the original array with the
-        // value at index j in the auxiliary array.
-        animations.push([k, auxiliaryArray[j]]);
-        mainArray[k++] = auxiliaryArray[j++];
-      }
-    }
-    while (i <= middleIdx) {
-      // These are the values that we're comparing; we push them once
-      // to change their color.
-      animations.push([i, i]);
-      // These are the values that we're comparing; we push them a second
-      // time to revert their color.
-      animations.push([i, i]);
+}
+
+function mergeSortHelper(
+  mainArray,
+  startIdx,
+  endIdx,
+  auxiliaryArray,
+  animations,
+) {
+  if (startIdx === endIdx) return;
+  const middleIdx = Math.floor((startIdx + endIdx) / 2);
+  mergeSortHelper(auxiliaryArray, startIdx, middleIdx, mainArray, animations);
+  mergeSortHelper(auxiliaryArray, middleIdx + 1, endIdx, mainArray, animations);
+  doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, animations);
+}
+
+function doMerge(
+  mainArray,
+  startIdx,
+  middleIdx,
+  endIdx,
+  auxiliaryArray,
+  animations,
+) {
+  let k = startIdx;
+  let i = startIdx;
+  let j = middleIdx + 1;
+  while (i <= middleIdx && j <= endIdx) {
+    // These are the values that we're comparing; we push them once
+    // to change their color.
+    animations.push([i, j]);
+    // These are the values that we're comparing; we push them a second
+    // time to revert their color.
+    animations.push([i, j]);
+    if (auxiliaryArray[i] <= auxiliaryArray[j]) {
       // We overwrite the value at index k in the original array with the
       // value at index i in the auxiliary array.
       animations.push([k, auxiliaryArray[i]]);
       mainArray[k++] = auxiliaryArray[i++];
-    }
-    while (j <= endIdx) {
-      // These are the values that we're comparing; we push them once
-      // to change their color.
-      animations.push([j, j]);
-      // These are the values that we're comparing; we push them a second
-      // time to revert their color.
-      animations.push([j, j]);
+    } else {
       // We overwrite the value at index k in the original array with the
       // value at index j in the auxiliary array.
       animations.push([k, auxiliaryArray[j]]);
       mainArray[k++] = auxiliaryArray[j++];
     }
   }
+  while (i <= middleIdx) {
+    // These are the values that we're comparing; we push them once
+    // to change their color.
+    animations.push([i, i]);
+    // These are the values that we're comparing; we push them a second
+    // time to revert their color.
+    animations.push([i, i]);
+    // We overwrite the value at index k in the original array with the
+    // value at index i in the auxiliary array.
+    animations.push([k, auxiliaryArray[i]]);
+    mainArray[k++] = auxiliaryArray[i++];
+  }
+  while (j <= endIdx) {
+    // These are the values that we're comparing; we push them once
+    // to change their color.
+    animations.push([j, j]);
+    // These are the values that we're comparing; we push them a second
+    // time to revert their color.
+    animations.push([j, j]);
+    // We overwrite the value at index k in the original array with the
+    // value at index j in the auxiliary array.
+    animations.push([k, auxiliaryArray[j]]);
+    mainArray[k++] = auxiliaryArray[j++];
+  }
+}
 
-  function quickSortHelper(
-    mainArray,
-    startIdx,
-    endIdx,
-    animations,
-  ) {
-    if (startIdx < endIdx) {
-      let pi = partition(mainArray, startIdx, endIdx, animations);
-      quickSortHelper(mainArray, startIdx, pi - 1, animations);
-      quickSortHelper(mainArray, pi + 1, endIdx, animations);
-    }
-  
-    return animations;
+function quickSortHelper(
+  mainArray,
+  startIdx,
+  endIdx,
+  animations,
+) {
+  if (startIdx < endIdx) {
+    let pi = partition(mainArray, startIdx, endIdx, animations);
+    quickSortHelper(mainArray, startIdx, pi - 1, animations);
+    quickSortHelper(mainArray, pi + 1, endIdx, animations);
   }
-  
-  function partition(
-    mainArray,
-    startIdx,
-    endIdx,
-    animations
-  ) {
-    const pivot = mainArray[endIdx];
-    let i = startIdx - 1;
-    for (let j = startIdx; j < endIdx; j++) {
-      // If current element is smaller than the pivot 
-      if (mainArray[j] < pivot) {
-        i = i + 1;
-  
-        // swap arr[i] and arr[j] 
-        const temp = mainArray[i];
-        mainArray[i] = mainArray[j];
-        mainArray[j] = temp;
-        animations.push([i, j, mainArray[i], mainArray[j]]);
-      }
+
+  return animations;
+}
+
+function partition(
+  mainArray,
+  startIdx,
+  endIdx,
+  animations
+) {
+  const pivot = mainArray[endIdx];
+  let i = startIdx - 1;
+  for (let j = startIdx; j < endIdx; j++) {
+    // If current element is smaller than the pivot 
+    if (mainArray[j] < pivot) {
+      i = i + 1;
+
+      // swap arr[i] and arr[j] 
+      const temp = mainArray[i];
+      mainArray[i] = mainArray[j];
+      mainArray[j] = temp;
+      animations.push([i, j, mainArray[i], mainArray[j]]);
     }
-  
-    // swap arr[i+1] and arr[high] (or pivot) 
-    const temp = mainArray[i + 1];
-    mainArray[i + 1] = mainArray[endIdx];
-    mainArray[endIdx] = temp;
-    animations.push([i + 1, endIdx, mainArray[i + 1], mainArray[endIdx]]);
-  
-  
-    return i + 1;
   }
-  
+
+  // swap arr[i+1] and arr[high] (or pivot) 
+  const temp = mainArray[i + 1];
+  mainArray[i + 1] = mainArray[endIdx];
+  mainArray[endIdx] = temp;
+  animations.push([i + 1, endIdx, mainArray[i + 1], mainArray[endIdx]]);
+
+
+  return i + 1;
+}
